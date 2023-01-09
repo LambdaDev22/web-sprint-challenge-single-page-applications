f831cab0e7abf6fb3d94410096b0b3408adef69f
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2023-01-09 15:52:23 -0600

    completing App.js, I think

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Home from "./components/Home";
+import { Route, Link, Switch } from 'react-router-dom';
 
 const App = () => {
+
+  const [errors, setErrors] = useState({
+    name: '',
+    size: '',
+    pepperoni: '',
+    olives: '',
+    bacon: '',
+    onions: '',
+    special: ''
+  });
+
+  const [form, setForm] = useState({
+    name: '',
+    size: '',
+    pepperoni: false,
+    olives: false,
+    bacon: false,
+    onions: false,
+    special: ''
+  })
+
+  const [order, setOrder] = useState([]);
+  const [disabled, setDisabled] = useState(true);
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Header />
+      <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/pizza'>
+            <Form
+              form={form} setForm={setForm}
+              errors={errors} setErrors={setErrors}
+              disabled={disabled} setDisabled={disabled}
+              order={order} setOrder={setOrder}
+            />
+          </Route>
+      </Switch>
     </>
   );
 };
